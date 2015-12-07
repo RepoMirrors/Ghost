3d874b346ea702b0f9a9b97062d057b046971b4e
小可先森 <imxieke@qq.com>
2015-12-07 23:06:22 +0800

    update generate-stackbrew-library.sh

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 url='git://github.com/docker-library/ghost'
 
-echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
+echo '# maintainer: Xiekers <imxieke@qq.com>'
 
 commit="$(git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
 fullVersion="$(grep -m1 'ENV GHOST_VERSION ' Dockerfile | cut -d' ' -f3)"
